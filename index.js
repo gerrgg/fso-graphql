23dbfd825c19ba24fa0301e609b9233f611efcce
Greg Bastianelli <greg@gerrg.com>
2020-12-11 13:10:54 -0500

    8.4: Books of an author

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -108,7 +108,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) =>
+      !args.author
+        ? books
+        : books.filter((book) => book.author === args.author),
     allAuthors: () => authors,
   },
 
