366a1c458d767acb122c5b80704dff11f540f1c9
Greg Bastianelli <greg@gerrg.com>
2020-12-11 12:43:49 -0500

    8.2 All books

--- index.js
+++ index.js
@@ -84,9 +84,17 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    author: String!
+    published: String!
+    genres: [String]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
@@ -94,6 +102,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
